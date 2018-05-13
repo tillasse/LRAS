23e10861a726d6555bc10e97011372726578f44c
tillasse <mojemmi.said@gmail.com>
2018-05-13 01:16:54 +0100

    Add files via upload

---
+++ Perl/bin/encode-base64.bat
@@ -0,0 +1,29 @@
+@rem = '--*-Perl-*--
+@echo off
+if "%OS%" == "Windows_NT" goto WinNT
+perl -x -S "%0" %1 %2 %3 %4 %5 %6 %7 %8 %9
+goto endofperl
+:WinNT
+perl -x -S %0 %*
+if NOT "%COMSPEC%" == "%SystemRoot%\system32\cmd.exe" goto endofperl
+if %errorlevel% == 9009 echo You do not have Perl in your PATH.
+if errorlevel 1 goto script_failed_so_exit_with_non_zero_val 2>nul
+goto endofperl
+@rem ';
+#!/usr/bin/perl
+#line 15
+
+use MIME::Base64 qw(encode_base64);
+
+my $buf = "";
+while (<>) {
+    $buf .= $_;
+    if (length($buf) >= 57) {
+	print encode_base64(substr($buf, 0, int(length($buf) / 57) * 57, ""));
+    }
+}
+
+print encode_base64($buf);
+
+__END__
+:endofperl
